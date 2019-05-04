70d45f419b76734cbe9e872398f340dff50bbdb4
coip <e@coip.me>
2019-05-04 16:29:26 -0500

    introduces asynchronicity to Gzip(): adds type CompressionJob and func (cj) Observe()

--- compression/compression.go
+++ compression/compression.go
@@ -5,20 +5,103 @@ import (
 	"compress/gzip"
 	"io"
 	"io/ioutil"
+	"log"
 )
 
-//Gzip the data from in r into w
-func Gzip(w io.Writer, r *bytes.Buffer) (int, error) {
+const buffersize = 8
+
+type (
+	//Rchunks transports the size of bytes a Read() operation returns, from the consuming goroutine to the caller asynchronously.
+	rchunks <-chan int
+	//Wchunks transports the size of bytes a Write() operation returns, from the consuming goroutine to the caller asynchronously.
+	wchunks <-chan int
+	//CompletionSignal receives true on complete. a false indicates an underlying irrecoverable failure has been detected for the processing.
+	completionSignal <-chan bool
+)
+
+type CompressionJob struct {
+	R    rchunks
+	W    wchunks
+	Done completionSignal
+	E    <-chan error
+}
+
+func (c CompressionJob) Observe() {
+	var (
+		read, written int
+		err           error
+		success       bool
+	)
+	go func() {
+		for {
+			select {
+			case read = <-c.R:
+				log.Printf("read %d bytes", read)
+			case written = <-c.W:
+				log.Printf("written %d bytes", written)
+			case err = <-c.E:
+				if err != io.EOF {
+					log.Fatalf("error detected while observing compression: %+v", err)
+					return
+				} else {
+					log.Println("EOF reached!")
+					return
+				}
+			case success = <-c.Done:
+				log.Printf("success: %t", success)
+			}
+		}
+	}()
+}
+
+//Gzip the data stored in src into dst
+func Gzip(w io.Writer, r io.Reader) CompressionJob {
+	var (
+		read, written int
+		e             error
+
+		buf = make([]byte, buffersize)
+
+		rq     = make(chan int, 1000)
+		wq     = make(chan int, 1000)
+		signal = make(chan bool)
+
+		errorQueue = make(chan error, 1)
+	)
+
 	gw, err := gzip.NewWriterLevel(w, gzip.BestCompression)
 	if err != nil {
 		panic(err)
 	}
-	defer gw.Close()
-	return gw.Write(r.Bytes())
+
+	go func(a chan int, b chan int, c chan bool, d chan error) {
+		for {
+			read, e = r.Read(buf)
+			if e == io.EOF {
+				c <- true
+				d <- io.EOF
+				gw.Close()
+				close(a)
+				close(b)
+				close(c)
+				close(d)
+				return
+			}
+			a <- read
+			written, e = gw.Write(buf)
+			if e != nil {
+				d <- e
+			}
+			b <- written
+		}
+	}(rq, wq, signal, errorQueue)
+
+	return CompressionJob{rchunks(rq), wchunks(wq), completionSignal(signal), errorQueue}
 }
 
-//Gunzip the data from in r into w
+//Gunzip the data stored in src into dst
 func Gunzip(w io.Writer, r *bytes.Buffer) error {
+	// Write gzipped data from src to dest
 	gr, err := gzip.NewReader(r)
 	if err != nil {
 		return err
